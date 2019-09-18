ae36522506b580cac70eef47f35c71d5d6c673dc
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-09-18 22:27:08 +0300

    Add redux

---
+++ client/src/redux/postsRedux.js
@@ -0,0 +1,37 @@
+/* SELECTORS */
+export const getPosts = ({ posts }) => posts;
+
+/* ACTIONS */
+export const LOAD_POSTS = createActionName('LOAD_POSTS');
+export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
+
+/* THUNKS */
+export const loadPostsRequest = () => {
+    return dispatch => {
+  
+      console.log('Request started...');
+      setTimeout(() => {
+        const arr = [{ id: 'a3fssdc1', title: 'Test', content: 'Lorem Ipsum' }];
+        dispatch(loadPosts(arr));
+        console.log('Request finished after 2sec!');
+      }, 2000);
+  
+    };
+  };
+
+// action name creator
+const reducerName = 'posts';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+/* INITIAL STATE */
+const initialState = [];
+
+/* REDUCER */
+export default function reducer(statePart = initialState, action = {}) {
+  switch (action.type) {
+    case LOAD_POSTS:
+        return [ ...action.payload ];
+    default:
+      return statePart;
+  }
+};
